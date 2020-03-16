bd12a9e39c534dfe196d073e5d094d0571e96bcd
Chris Ventura <chrisv@iqmetrix.com>
2020-03-16 11:46:33 -0500

    refactor: Cleaned up code in native-components

--- packages/native-compoonents/src/button.ts
+++ packages/native-compoonents/src/button.ts
@@ -1,6 +1,7 @@
 export class Button {
     public onPress() { 
+        console.log("onpress to test");
         console.log("onpress");
-        console.log("onpress to test")
+        console.log("refactor");
     }
 }
